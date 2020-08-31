8d1f5abe85ffa96467d312b3a70e48ebb603d379
LeonChisum <leonchisum3306@gmail.com>
2020-08-30 20:19:12 -0400

    ring_buffer.py Implemented append and get methods all tests passed

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,22 @@
 class RingBuffer:
-    def __init__(self, capacity):
-        pass
+    def __init__(self, capacity=1):
+        self.capacity = capacity
+        self.items = []
+        self.oldest_item = 0
 
     def append(self, item):
-        pass
+        # checking to see if my list is below the capacity
+        if len(self.items) < self.capacity:
+            self.items.append(item)
+        #     once the list is at capacity
+        elif len(self.items) == self.capacity:
+            # replacing the item in the list at the index of the oldest item
+            self.items[self.oldest_item] = item
+            # setting the next oldest item index based on the given capacity
+            self.oldest_item = (self.oldest_item + 1) % self.capacity
 
     def get(self):
-        pass
+        if len(self.items) <= self.capacity:
+            return self.items
+        else:
+            return self.items[self.oldest_item:]+self.items[:self.oldest_item]
