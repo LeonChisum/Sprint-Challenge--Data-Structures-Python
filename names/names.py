528eb5f2f19fc0ad89cb3f8cd7d51460ea9325a3
LeonChisum <leonchisum3306@gmail.com>
2020-08-30 20:50:32 -0400

    names.py runtime reduced when finding duplicates

--- names/names.py
+++ names/names.py
@@ -13,10 +13,19 @@ f.close()
 duplicates = []  # Return the list of duplicates in this data structure
 
 # Replace the nested for loops below with your improvements
-for name_1 in names_1:
-    for name_2 in names_2:
-        if name_1 == name_2:
-            duplicates.append(name_1)
+# for name_1 in names_1:
+#     for name_2 in names_2:
+#         if name_1 == name_2:
+#             duplicates.append(name_1)
+names = {}
+# creating a dictionary which list all names in first List as keys
+for n in names_1:
+    names[n] = True
+# iterating over the list of names
+for n2 in names_2:
+    # checking to see if it exists as a key in dictionary vs looping 2 Lists.
+    if n2 in names:
+        duplicates.append(n2)
 
 end_time = time.time()
 print (f"{len(duplicates)} duplicates:\n\n{', '.join(duplicates)}\n\n")
